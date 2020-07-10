d743dc30ee33650b82964fd6553905840decdf55
Werlang.B <52296509+WerlangB@users.noreply.github.com>
2020-07-10 19:21:10 -0300

    update

--- pong.js
+++ pong.js
@@ -1,5 +1,6 @@
 let ctx, p1_y, p2_y, p1_points, p2_points
 let ball_y_orientation, ball_x_orientation, ball_x, ball_y
+let p1_key, p2_key
 const h=500, w=800, p_w=20, p_h=200, p1_x = 10, p2_x = w - p_w - 10
 function setup(){
     const canvas = document.getElementById("canvas")
@@ -19,7 +20,35 @@ function setup(){
 }
 
 function loop(){
+    //Verifica se a bola está colidindo com o barra do player 1
+    if(ball_x >= p1_x && ball_x <= p1_x + 10 && ball_y >= p1_y && ball_y <= p1_y + p_h){
+        ball_x_orientation = 1
+    }
+    //Verifica se a bola está colidindo com o barra do player 2
+    else if(ball_x >= p2_x && ball_x <= p2_x + 10 && ball_y >= p2_y && ball_y <= p2_y + p_h){
+        ball_x_orientation = -1
+    }
 
+    // verifica se a bola passou bateu no chão ou no teto
+    if(ball_y + 10 >= h || ball_y <= 0) ball_y_orientation *= -1
+
+    //move a bola no eixo X e Y
+    ball_x += 5 * ball_x_orientation
+    ball_y += 5 * ball_y_orientation
+
+
+    if(p1_key == 87 && p1_y > 0){
+        p1_y -= 10
+    }else if(p1_key == 83 && p1_y + p_h < h){
+        p1_y += 10
+    }
+
+    if(p2_key == 38 && p2_y > 0){
+        p2_y -= 10
+    }else if(p2_key == 40 && p2_y + p_h < h){
+        p2_y += 10
+    }
+    draw()
 }
 
 function initBall(){
@@ -48,5 +77,15 @@ function drawRect(x,y,w,h,color="#fff"){
     ctx.fillRect(x,y,w,h)
     ctx.fillStyle = "#000"
 }
+
+document.addEventListener("keydown",function(ev){
+    // keyCode 87 = w, keycode 83 = s
+    if(ev.keyCode == 87 || ev.keyCode == 83){
+        p1_key = ev.keyCode
+    }
+    // keycode 38 = arrowUp, keycode 40 = arrowDown
+    else if(ev.keyCode== 38 || ev.keyCode==40)
+        p2_key = ev.keyCode
+})
 
 setup()
